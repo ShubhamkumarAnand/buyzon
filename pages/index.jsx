b115f7442b336dcdf05caf0118d59c1c742b4f26
imskanand <the.anand3099@gmail.com>
2022-07-01 20:17:57 +0530

    Designed the Footer Banner Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,6 @@ import { client } from '../lib/client';
 const Home = ({ products, bannerData }) => (
   <>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-    {console.log(bannerData)}
     <div className="products-heading">
       <h2>Best selling products</h2>
       <p>Speakers of many variations</p>
@@ -12,7 +11,7 @@ const Home = ({ products, bannerData }) => (
     <div className="products-container">
       { products?.map((product) => <Product key={product._id} product={product} />) }
     </div>
-    <FooterBanner />
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </>
 );
 
