cf5e34828fe307bfd30b27b20b6e933cd39777e3
imskanand <the.anand3099@gmail.com>
2022-07-01 11:05:27 +0530

    Designed the Herobanner Outline

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
 import React from 'react';
 
 const HeroBanner = () => (
-  <div>HeroBanner</div>
+  <div className="hero-banner-container">
+    <div>
+      <p className="beats-solo">SMALL TEXT</p>
+      <h3>MID TEXT</h3>
+      <img src="" alt="headphones" className="hero-banner-image" />
+      <div>
+        <Link href="/products/ID">
+          <button type="button">
+            BUTTON TEXT
+          </button>
+        </Link>
+        <div className="DESC">
+          <h5>description</h5>
+          <p>DESCRIPTION</p>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default HeroBanner;
