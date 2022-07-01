902c506be4ccc0685ffa1a8722cc73942ee71147
imskanand <the.anand3099@gmail.com>
2022-07-01 12:22:31 +0530

    Fetching the data from the Sanity API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,31 @@
 import { Product, FooterBanner, HeroBanner } from '../components';
 import { client } from '../lib/client';
 
-const index = () => (
+const Home = ({ products, bannerData }) => (
   <>
     <HeroBanner />
+    {console.log(bannerData)}
     <div className="products-heading">
       <h2>Best selling products</h2>
       <p>Speakers of many variations</p>
     </div>
     <div className="products-container">
-      {['Product1', 'Product2'].map((product) => product)}
+      {products?.map((product) => product.name)}
     </div>
     <FooterBanner />
   </>
 );
 
-export default index;
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
