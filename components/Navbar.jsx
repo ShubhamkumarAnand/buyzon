7f932bf2c21f4fc5858f9d3b04e2dbef7e445ac9
imskanand <the.anand3099@gmail.com>
2022-07-02 11:36:11 +0530

    A Static implementatioin of the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { AiOutlineShopping } from 'react-icons/ai';
+import Link from 'next/link';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <div className="navbar-container">
+    <p className="logo">
+      <Link href="/">Buyzon Headphones</Link>
+    </p>
+    <button className="cart-icon" type="button" onClick="">
+      <AiOutlineShopping />
+      <span className="cart-item-qty">1</span>
+    </button>
+  </div>
 );
 
 export default Navbar;
