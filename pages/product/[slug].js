fbea2158953be9c9c3fe80ccdbd0d828402d5461
imskanand <the.anand3099@gmail.com>
2022-07-03 20:54:59 +0530

    getStaticProps is implemented

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { urlFor, client } from '../../lib/client';
 
 const ProductDetails = () => (
-  <div>Product</div>
+  <div>
+    <div className="product-detail-container">
+      <div>
+        <div className="image-container">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
 );
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type=="product" && slug.current ==' ${slug}'][0]`;
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 
 export default ProductDetails;
