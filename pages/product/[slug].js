438a1d224daf9317f438053baa79cb7392f5a3b2
imskanand <the.anand3099@gmail.com>
2022-07-04 12:30:37 +0530

    Getting Out of error hell

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,27 +1,47 @@
 import React from 'react';
 import { urlFor, client } from '../../lib/client';
 
-const ProductDetails = () => (
-  <div>
-    <div className="product-detail-container">
-      <div>
-        <div className="image-container">
-          <img src="" alt="" />
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} alt="" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type=="product"]{
+  slug{
+    current
+  }
+}`;
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
 
 export const getStaticProps = async ({ params: { slug } }) => {
-  const query = `*[_type=="product" && slug.current ==' ${slug}'][0]`;
-  const products = await client.fetch(query);
-
-  const bannerQuery = '*[_type=="banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+  const query = `*[_type=="product" && slug.current =='${slug}'][0]`;
+  const productQuery = '*[_type=="product"]';
+  const product = await client.fetch(query);
+  const products = await client.fetch(productQuery);
 
   return {
-    props: { products, bannerData },
+    props: { product, products },
   };
 };
 
