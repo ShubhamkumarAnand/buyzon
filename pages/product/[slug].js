8fe00fb1799516bb53bc2f64d8e61e97fe7d7d74
imskanand <the.anand3099@gmail.com>
2022-07-06 10:12:45 +0530

    implemented image carousol

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,25 +1,27 @@
+import { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineStar, AiFillStar } from 'react-icons/ai';
 import { urlFor, client } from '../../lib/client';
 import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="" />
+            <img src={urlFor(image && image[index])} alt="" />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             { image?.map((item, i) => (
               <img
                 src={urlFor(item)}
-                className=""
-                onMouseEnter=""
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
-          </div> */}
+          </div>
         </div>
         <div className="product-detail-desc">
           <h1>{ name } </h1>
