fc8014463676c99283753898d8ccb389ce4c54fc
imskanand <the.anand3099@gmail.com>
2022-07-06 19:33:09 +0530

    Fixed Image carousol size on hover

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,7 +11,7 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} alt="" />
+            <img src={urlFor(image && image[index])} alt={name} className="product-detail-image" />
           </div>
           <div className="small-images-container">
             { image?.map((item, i) => (
