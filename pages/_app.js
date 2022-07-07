1727a49f488e28f571bec261dfe8b729280e6575
imskanand <the.anand3099@gmail.com>
2022-07-07 12:22:25 +0530

    Designing context and passing the data in the main app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import { Toaster } from 'react-hot-toast';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 const MyApp = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <StateContext>
+    <Layout>
+      <Toaster />
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
+
 );
 
 export default MyApp;
